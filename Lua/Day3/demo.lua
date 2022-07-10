21b629c95cf083546093b9c2e95f1e0ad2a47f69
Gurparv Singh <gurparv@gmail.com>
2022-07-10 23:53:45 +0530

    Day3 Logical operators code

---
+++ Lua/Day3/demo.lua
@@ -0,0 +1,13 @@
+
+ -- Logical Operators:
+ 
+human_property1 = "Cash"
+human_property2 = "Big House" 
+
+if human_property1 == "Cash" or human_property1 == "Car" then
+	print("Human is rich")
+end
+
+if human_property1 == "Cash" and human_property2 =="Big House" then 
+	print("Human is super rich")
+end
