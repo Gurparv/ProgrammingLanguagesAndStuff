91d0eb8c09e98ddbcedb045d5a28d705d1d007c9
Gurparv Singh <gurparv@gmail.com>
2022-07-12 01:55:33 +0530

    Add day 4 lua code

---
+++ Lua/Day4/demo.lua
@@ -0,0 +1,7 @@
+
+-- Strings and Standard Library modules
+
+name = "John"
+lowercase_name = string.lower(name)
+print(lowercase_name)
+print(name)  -- original string is not changed as strings are immutable
