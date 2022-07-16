8c86f08b418669699ba5959442082b1cce54ee66
Gurparv Singh <gurparv@gmail.com>
2022-07-17 01:16:30 +0530

    Add Flappy + Nodejs learnings

---
+++ PersonalProjects/TUI_FlappyBird/Main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+printf("Hello World \n");
+//Creates 2 Walls that never changes
+for(int i=0;i<80;i++){ printf("*"); }
+printf("\n");
+for(int i=0;i<80;i++){ printf("*"); }
+//system("clear");
+}
