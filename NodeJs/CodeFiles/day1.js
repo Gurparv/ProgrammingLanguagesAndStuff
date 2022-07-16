732e19e36f93984840b84478649a6c5ea820aebc
Gurparv Singh <gurparv@gmail.com>
2022-07-17 01:19:17 +0530

    Add all changes

--- NodeJs/CodeFiles/day1.js
+++ NodeJs/CodeFiles/day1.js
@@ -11,20 +11,42 @@ console.log(hello);
 
 const fs = require('fs');
 
-const data = fs.readFileSync('../abc.txt','utf-8');
-console.log(data); //prints data on the console.
+//Reading from file Synchronously, Blocking way.
+//const data = fs.readFileSync('../abc.txt','utf-8');
+//console.log(data); //prints data on the console.
 
-//Writing to file
+//Writing to file Synchronously , Blocking way.
 const dataOut = "This text should be written to file";
 //const dataOut = `This text should be mixed with ${data} and ${Date.now()}`;  //Use of backticks to format text with other js variables and Js inbuilt function return values
 
 fs.writeFileSync('./output.txt',dataOut);
 console.log('File is written');
 
+            /////////////////////////////////////////////             
+
+//Reading from file Asynchronously, Non-Blocking way.
+fs.readFile("\\Users\\manji\\Desktop\\dummy.txt","utf-8",(err,myData)=>{ 	//This weird thing is callback function 
+console.log(`${myData} 😁`);
+});
+console.log("This should be read at last"); // This gets printed before the above line becoz of Asynchronous
+
+	//Just another example of asyc reading of file - here reading a file, which contains name of another file and then we are reading that 2nd file too
+fs.readFile("\\Users\\manji\\Desktop\\iHaveAFileName.txt","utf-8",(err,myData)=>{ 	
+	fs.readFile(`\\Users\\manji\\Desktop\\${myData}.txt`,"utf-8",(err,myData2)=>{
+		console.log(`File name was: ${myData}📌  \nData in that file was:\n \t ${myData2}`);
+	});
+});
+console.log("This should be read at last"); // This gets printed before the above line becoz of Asynchronous
+
+//Writing to file Aynchronously, Non-Blocking way
+const inputData = "I am learning NodeJs";
+fs.writeFile("\\Users\\manji\\Desktop\\nodeDummy.txt",inputData,"utf-8",(err) => {
+	console.log("Written into file asynchronously.!");
+});
 
 ////////////////////////////////////////////////////////////////////////////////
 
-
+/**** Importing Modules ****/
 
 
 
