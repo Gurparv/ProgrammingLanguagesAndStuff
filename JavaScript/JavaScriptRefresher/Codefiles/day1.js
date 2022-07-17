5f8dad85801f8e042bf87c78ea2b7ffda2c35d51
Gurparv Singh <gurparv@gmail.com>
2022-07-18 01:00:19 +0530

    Add code changes of reactlearning and javascript refresher

---
+++ JavaScript/JavaScriptRefresher/Codefiles/day1.js
@@ -0,0 +1,15 @@
+/* let and const */
+let name = "Sasha";
+console.log(name);
+
+name = "Betty"; //Will work fine.
+
+
+const myname = "John";
+console.log(myname);
+
+myname = "something"; //Error as trying to assign another value to a const variable.
+////////////////////////////////////////////////////////////////////////////////
+
+
+
---
+++ JavaScript/JavaScriptRefresher/JSRefresherNotes.md
@@ -0,0 +1,11 @@
+## Next-Gen Javascript or ES6 javascript.
+
+### Topic1: Understanding "let" and "const" 
+Declaring variables can be done using:
+1. var (Old, not recommended)
+2. let (New, recommended) -> for variables that change values.
+3. const (New, recommended) -> for constants.
+
+
+### Topic2: Arrow Functions.
+
